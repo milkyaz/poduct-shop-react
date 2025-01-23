e75b24625755e0c569b574c3555f518d856c573f
Daniil Zaburunov <zaburunov.2000@mail.ru>
2025-01-23 22:01:47 +0400

    update

--- src/components/GoodsList/GoodsList.jsx
+++ src/components/GoodsList/GoodsList.jsx
@@ -1,26 +1,41 @@
 import GoodsItem from "../GoodsItem/GoodsItem";
 import Box from "@mui/material/Box";
+import "./GoodsList.css";
 
 export default function GoodsList(props) {
   const { goods = [] } = props;
 
   {
     return (
-      <Box
-        className="box__item"
-        style={{
-          display: "flex",
-          gap: 40,
-          flexWrap: "wrap",
-          marginTop: 50,
-          width: "916px",
-          border: "1px solid",
-        }}
-      >
-        {goods.map((product) => (
-          <GoodsItem key={product.id} {...product} />
-        ))}
-      </Box>
+      <>
+        <div className="filter-panel">
+          <div className="filter-panel__text">
+            <h3 className="filter-panel__text-title">Категории товаров</h3>
+            <h3 className="filter-panel__text-right">Настройки</h3>
+          </div>
+          <div className="filter-panel__tags">
+            <p className="filter-panel__tags__elem-blue">Мужская одежда</p>
+            <p className="filter-panel__tags__elem-green">Ювелирка</p>
+            <p className="filter-panel__tags__elem-orange">Электроника</p>
+            <p className="filter-panel__tags__elem-pink">Женская одежда</p>
+          </div>
+        </div>
+        <Box
+          className="box-shop"
+          style={{
+            display: "flex",
+            gap: 40,
+            flexWrap: "wrap",
+            marginTop: 50,
+            width: "916px",
+            border: "1px solid",
+          }}
+        >
+          {goods.map((product) => (
+            <GoodsItem key={product.id} {...product} />
+          ))}
+        </Box>
+      </>
     );
   }
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,33 @@
+import "./Sidebar.css";
+
 export default function Sidebar() {
+  const IMAGES = {
+    image1: new URL("./img/Frame.png", import.meta.url).href,
+  };
+
   return (
-    <div
-      className="sidebar"
-      style={{ width: "351px", border: "1px solid", marginTop: "27px" }}
-    >
-      
-      div
+    <div className="sidebar">
+      <div className="sidebar-box">
+        <div className="sidebar-box__element-left">
+          <img
+            style={{
+              width: "91px",
+              height: "82px",
+              position: "relative",
+              right: "17px",
+            }}
+            src={IMAGES.image1}
+            alt="скидки"
+          />
+        </div>
+        <div className="sidebar-box__element-right">
+          <h3 className="sidebar-box__element-right__text">Получай товары</h3>
+          <h3 className="sidebar-box__element-right__text">Бесплатно!</h3>
+          <button className="sidebar-box__element-right__button">
+            Узнать подробнее
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
